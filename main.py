571284962561456c52fdb1c0b02a2aaa123cd9e5
leoaob <leoaob@gmail.com>
2024-09-08 10:15:48 +0100

    1 commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Test")
+
+#variaveis
+idade = 47
+nome = "Leo"
+
+print(idade)
+
+#soma
+nova_idade = idade+1
+
+print(nova_idade)
+
+
+lista = [1,2,3,4,5]
+
+lista_invertida = lista.reverse()
+
+print(lista_invertida)
